bbb46a209cce161174d77debdb11ae9bcfe5ce79
Isla <BeepIsla@gmail.com>
2019-04-05 12:50:26 +0200

    Major update

---
+++ helpers/Discord.js
@@ -0,0 +1,38 @@
+const DDiscord = require("discord-rpc");
+
+module.exports = class Discord {
+	constructor(clientId) {
+		this.clientId = clientId;
+		this.client = new DDiscord.Client({ transport: "ipc" });
+		this.pid = null;
+	}
+
+	stop() {
+		if (this.client.user !== null) {
+			this.client.connectTime = null;
+			this.client.destroy();
+
+			this.pid = null;
+		}
+	}
+
+	async start(pid, activity = null) {
+		this.pid = pid;
+
+		await this.client.login({ clientId: this.clientId });
+		this.client.connectTime = Math.round(Date.now() / 1000);
+
+		if (typeof activity !== null) {
+			await this.client.setActivity(activity, this.pid);
+		}
+	}
+
+	// TODO: Add queue system for activities so I don't have to care about spamming it
+	async setActivity(obj) {
+		if (this.client.user === null) {
+			return;
+		}
+
+		return await this.client.setActivity(obj, this.pid);
+	}
+}
---
+++ helpers/Process.js
@@ -0,0 +1,72 @@
+const childProcess = require("child_process");
+const Events = require("events");
+
+module.exports = class Process extends Events {
+	constructor(process, checkInterval) {
+		super();
+
+		this.checkInterval = checkInterval;
+		this.process = process;
+		this.running = false;
+		this.curPID = null;
+
+		// Continously loop
+		this.interval = setInterval(async () => {
+			let PID = await this.isRunning().catch(() => {});
+			if (typeof PID === "undefined") {
+				if (this.running === false) {
+					return;
+				}
+
+				this.running = false;
+				this.curPID = null;
+				this.emit("stopped");
+				return;
+			}
+
+			if (this.running === true) {
+				return;
+			}
+
+			this.running = true;
+			this.curPID = parseInt(PID);
+			this.emit("running", this.curPID);
+		}, this.checkInterval);
+	}
+
+	/**
+	 * Checks if the passed configuration is running or not
+	 * @returns {Promise}
+	 */
+	isRunning() {
+		return new Promise((resolve, reject) => {
+			childProcess.exec("tasklist", { windowsHide: true }, async (err, stdout, stderr) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+
+				let lines = stdout.split("\n");
+				for (let line of lines) {
+					let match = line.trim().match(/^(?<process>.+\.exe)\s+(?<pid>\d+)\s+(.+)\s+(\d+)\s+(\d+(\.\d+|){0,})\s+[A-Z]+$/);
+					if (match === null) {
+						continue;
+					}
+
+					if (match.groups === null) {
+						continue;
+					}
+
+					if (match.groups.process.toLowerCase() !== this.process.toLowerCase()) {
+						continue;
+					}
+
+					resolve(match.groups.pid);
+					return;
+				}
+
+				reject(stderr.length > 0 ? new Error(stderr) : new Error("Failed to get tasklist"));
+			});
+		});
+	}
+}
