b089c929f806c4bf3ceed4fefb965bad95e29320
Felix <DukeBruno123@gmail.com>
2019-04-06 12:27:52 +0200

    Fixes

--- helpers/Discord.js
+++ helpers/Discord.js
@@ -3,14 +3,15 @@ const DDiscord = require("discord-rpc");
 module.exports = class Discord {
 	constructor(clientId) {
 		this.clientId = clientId;
-		this.client = new DDiscord.Client({ transport: "ipc" });
+		this.client = null;
 		this.pid = null;
 	}
 
 	stop() {
-		if (this.client.user !== null) {
+		if (this.client !== null) {
 			this.client.connectTime = null;
 			this.client.destroy();
+			this.client = null;
 
 			this.pid = null;
 		}
@@ -19,16 +20,22 @@ module.exports = class Discord {
 	async start(pid, activity = null) {
 		this.pid = pid;
 
+		if (this.client === null) {
+			this.client = new DDiscord.Client({ transport: "ipc" });
+		}
+
 		await this.client.login({ clientId: this.clientId });
 		this.client.connectTime = Math.round(Date.now() / 1000);
 
 		if (typeof activity !== null) {
+			activity.startTimestamp = this.client.connectTime;
+
 			await this.client.setActivity(activity, this.pid);
 		}
 	}
 
 	async setActivity(obj) {
-		if (this.client.user === null) {
+		if (this.client === null) {
 			return;
 		}
 
